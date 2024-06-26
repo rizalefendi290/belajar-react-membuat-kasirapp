40833158926ae873c06107d2e6aef53d26234041
Rizal Efendi <157675553+rizalefendi290@users.noreply.github.com>
2024-06-26 23:46:09 +0700

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import NavbarComponents from './components/NavbarComponents';
+
+function App() {
+  return (
+    <div className="App">
+      <NavbarComponents/>
+        <h2>test</h2>
+    </div>
+  );
+}
+
+export default App;
